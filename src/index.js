c1b121ce9a898d0128fe816e161df969dac98813
martaclj <marlarrad@gmail.com>
2022-11-05 16:29:03 +0100

    Execute timer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const secondsSpan = document.querySelector("#seconds");
 const minutesSpan = document.querySelector("#minutes");
+const timerButton = document.querySelector("#timmer--button");
+const hero = document.querySelector("#principal");
 let secondsValue = 0;
 let minutesValue = 0;
 let currentChronometer;
@@ -24,7 +26,9 @@ function formatValue(value){
 }
 
 function stopChronometer(){
-  currentButton.disabled = false;
+  if (currentButton) {
+    currentButton.disabled = false;
+  }
   clearInterval(currentChronometer);
 }
 
@@ -33,4 +37,18 @@ function resetChronometer(){
   minutesValue = 0;
   secondsSpan.textContent = "00";
   minutesSpan.textContent = "00";
+}
+
+function executeTimer(){
+  hero.innerHTML = `
+  <h1 class=hero--title>Timer</h1>
+  <div class="hero--time">
+    <p id="time"><span id="minutes">00</span>:<span id="seconds">00</span></p>
+  </div>
+  <div class="hero--buttons">
+    <button onclick="startChronometer()" class="button hero--button" type="button">Start</button>
+    <button onclick="stopChronometer()" class="button hero--button" type="button">Stop</button>
+    <button onclick="resetChronometer()" class="button hero--button" type="button">Reset</button>
+  </div>
+  `
 }
